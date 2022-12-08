7eb35a17aeb0507dba60f579d98f5ecf3f3499c8
KhanhHuyen511 <nkhanhhuyen511@gmail.com>
2022-12-08 23:05:39 +0700

    .

--- FlowerShopManagement.Web/Areas/Admin/Controllers/OrderController.cs
+++ FlowerShopManagement.Web/Areas/Admin/Controllers/OrderController.cs
@@ -10,5 +10,10 @@ namespace FlowerShopManagement.Web.Areas.Admin.Controllers
             ViewBag.Order = true;
             return View();
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
--- FlowerShopManagement.Web/Areas/Admin/Controllers/ProductController.cs
+++ FlowerShopManagement.Web/Areas/Admin/Controllers/ProductController.cs
@@ -10,5 +10,10 @@ namespace FlowerShopManagement.Web.Areas.Admin.Controllers
             ViewBag.Product = true;
             return View();
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
