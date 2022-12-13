3b057fb68661ea83b3a093e63e33731d99eee9b9
phatlam1811@gmail.com <phatlam1811@gmail.com>
2022-12-13 15:56:38 +0700

    ImportController - Relatively completed

--- FlowerShopManagement.WebAPI/Controllers/ImportController.cs
+++ FlowerShopManagement.WebAPI/Controllers/ImportController.cs
@@ -16,15 +16,18 @@ public class ImportController : Controller
     private readonly IStaffService _staffService;
     private readonly IStockService _stockService;
     private readonly IProductRepository _productRepository;
+    private readonly IMailService _mailService;
 
     public ImportController(
         IStaffService staffService,
         IStockService stockService,
-        IProductRepository productRepository)
+        IProductRepository productRepository,
+        IMailService mailService)
     {
         _staffService = staffService;
         _stockService = stockService;
         _productRepository = productRepository;
+        _mailService = mailService;
     }
 
     // ============ IMPORT PAGE ============
@@ -38,46 +41,41 @@ public class ImportController : Controller
         // Parse to view model
         var viewModel = new ImportIndexVM();
         viewModel.LowOnStockProductModels = lowOnStockProducts;
-        viewModel.suppliers = suppliers;
+        viewModel.Suppliers = suppliers;
 
         return viewModel;
 
         //return View(viewModel);
     }
 
-    // ============ CREATE SUPPLY FORM ACTION ============
-    //[HttpPost]
-    //public SupplyFormModel /*IActionResult*/ CreateSupplyForm()
-    //{
-    //    return new NotImplementedException();
-    //}
+    // ============ CREATE & REVIEW SUPPLY FORM ACTION ============
+    [HttpPost]
+    public SupplyFormModel?/*IActionResult*/ CreateSupplyForm(ImportIndexVM viewModel)
+    {
+        var selectedItems = viewModel.SelectedItems;
+        var selectedSuppliers = viewModel.SelectedSuppliers;
+
+        // Requested items and suppliers must not be null
+        if (selectedItems == null || selectedSuppliers == null) return null;
+
+        // Fill in the form model
+        var supplyForm = new SupplyFormModel(selectedItems, selectedSuppliers);
+
+        return supplyForm;
+                    
+        //return View(supplyForm);
+    }
 
     // ============ REQUEST SUPPLY ACTION ============
-
-    // Action performed after the user clicks the "Create Form" button
-    //[HttpGet]
-    //public SupplyFormModel/*IActionResult*/ CreateSupplyRequestForm(List<LowOnStockProductModel> supplyList, List<SupplierModel> supplierList)
-    //{
-    //    var supplyFormModel = new SupplyFormModel(supplyList, supplierList);
-
-    //    return supplyFormModel;
-    //}
-
-    // Action performed after user clicks the "Request" button
     [HttpPost]
-    public bool/*IActionResult*/ RequestSupply(SupplyFormModel supplyFormModel)
+    public async Task<IActionResult> RequestSupply(SupplyFormModel supplyForm)
     {
-        try
-        {
-            //_importServices.Request(supplyFormModel);
-            
-            // Successfully requested
-            return true;
-        }
-        catch
-        {
-            // Failed to request
-            return false;
-        }
+        // Model to message
+        var mimeMessage = _mailService.CreateMimeMessage(supplyForm);
+
+        // Send the request message to suppliers
+        await _mailService.Send(mimeMessage);
+
+        return View();
     }
 }
