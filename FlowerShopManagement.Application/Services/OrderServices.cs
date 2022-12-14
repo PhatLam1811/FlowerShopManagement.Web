c32c23550888d97de45f60b29f955b7c06c04c92
lehquy13 <lehquy13@gmail.com>
2022-12-14 11:22:31 +0700

    update orderServices

--- FlowerShopManagement.Application/Services/OrderServices.cs
+++ FlowerShopManagement.Application/Services/OrderServices.cs
@@ -14,24 +14,56 @@ namespace FlowerShopManagement.Core.Services;
 
 public class OrderServices : IOrderServices
 {
-    public async Task<bool> CreateOrder(OrderModel order, UserModel user, IOrderRepository orderRepository,
+    public async Task<bool> CreateOrder(OrderModel order, UserModel currentUser, IOrderRepository orderRepository,
         IUserRepository userRepository, IProductRepository productRepository)
     {
         //Create OrderEntity object
         var newOrder = order.ToEntity();
-        var cus = await userRepository.GetByEmailOrPhoneNb(user.Email);
+        var cus = await userRepository.GetByEmailOrPhoneNb(currentUser.PhoneNumber);
         if (cus != null)
         {
             newOrder._accountID = cus._id;
             newOrder._phoneNumber = cus.phoneNumber;
             newOrder._customerName = cus.name;
         }
+        else
+        {
+            return false;
+        }
 
         if (newOrder != null && newOrder._id != null)
         {
+            List<Product> updateProductList = new List<Product>();
 
+            if (newOrder._products != null)
+            {
+                // reduce the amount of product before updating
+                foreach (var item in newOrder._products)
+                {
+                    if (item != null && item._id != null)
+                    {
+                        var product = await productRepository.GetById(item._id);
+                        //Amount unavaibale => false
+                        if (product._amount < item._amount) return false;
+                        product._amount -= item._amount;
+                        //Add to updateProList
+                        updateProductList.Add(product);
+                    }
+
+                }
+            }
+            // Updating...
+            foreach (var item in updateProductList)
+            {
+                if (item != null && item._id != null)
+                {
+                    var updateResult = await productRepository.UpdateById(item._id, item);
+                    if (!updateResult)
+                        return false;
+                }
+            }
             // Successful case happens
-            newOrder._status = Status.Waiting;//On waiting
+            newOrder._status = Status.Waiting;//On waiting for confirm
             var result = await orderRepository.Add(newOrder);
             return result;
 
