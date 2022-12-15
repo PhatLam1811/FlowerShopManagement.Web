c2c6fffa61bfce5a9098d6c9757d924da9e1c495
KhanhHuyen511 <nkhanhhuyen511@gmail.com>
2022-12-15 22:29:26 +0700

    .

--- FlowerShopManagement.Application/Interfaces/IOrderServices.cs
+++ FlowerShopManagement.Application/Interfaces/IOrderServices.cs
@@ -8,5 +8,4 @@ namespace FlowerShopManagement.Application.Interfaces;
 public interface IOrderServices
 {
     public Task<bool> CreateOrder(OrderModel order, UserModel currentUser, IOrderRepository orderRepository, IUserRepository userRepository, IProductRepository productRepository);
-
-
+}
--- FlowerShopManagement.Web/Controllers/ProfileController.cs
+++ FlowerShopManagement.Web/Controllers/ProfileController.cs
@@ -1,25 +1,72 @@
-﻿using FlowerShopManagement.Application.Models;
+﻿using FlowerShopManagement.Application.Interfaces;
+using FlowerShopManagement.Application.Interfaces.UserSerivices;
+using FlowerShopManagement.Application.Models;
+using FlowerShopManagement.Application.MongoDB.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FlowerShopManagement.Web.Controllers;
 
 public class ProfileController : Controller
 {
-    public IActionResult Index()
+    private readonly IAuthService _authServices;
+    private readonly IUserService _userService;
+    private readonly IUserRepository _userRepository;
+    private readonly ILogger<ProfileController> _logger;
+
+    public ProfileController(IAuthService authServices, ILogger<ProfileController> logger, IUserRepository userRepository, IUserService userService)
+    {
+        _authServices = authServices;
+        _logger = logger;
+        _userRepository = userRepository;
+        _userService = userService;
+    }
+
+    public async Task<IActionResult> Index()
     {
         ViewBag.Profile = true;
-        return View();
+
+        var user = await _userRepository.GetByEmailOrPhoneNb("jah@gmail.com");
+
+        // Create UserModel
+        UserDetailsModel user1 = new UserDetailsModel(user);
+
+        return View(user1);
     }
 
     [HttpGet]
-    public IActionResult PersonalInformation()
+    public async Task<IActionResult> PersonalInformation()
     {
-        return PartialView("PersonalInformation");
+        var user = await _userRepository.GetByEmailOrPhoneNb("jah@gmail.com");
+
+        // Create UserModel
+        UserDetailsModel user1 = new UserDetailsModel(user);
+
+        return PartialView("PersonalInformation", user1);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PersonalInformation(UserDetailsModel model)
+    {
+        var user = await _userRepository.GetByEmailOrPhoneNb("jah@gmail.com");
+
+        // edit
+
+        return PartialView("PersonalInformation", model);
     }
 
     [HttpGet]
     public IActionResult ChangePassword()
     {
+        return PartialView("ChangePassword", new ChangePasswordModel());
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> ChangePassword(ChangePasswordModel model)
+    {
+        var user = await _userRepository.GetByEmailOrPhoneNb("jah@gmail.com");
+
+        // Check old password
+
         return PartialView("ChangePassword", new ChangePasswordModel());
     }
 
