0fde327209eaf6c719fa986c5779cc47497fc35d
phatlam1811@gmail.com <phatlam1811@gmail.com>
2023-01-26 04:56:03 +0700

    ImportRepository - Add

---
+++ FlowerShopManagement.Application/Interfaces/MongoDB/IImportRepository.cs
@@ -0,0 +1,8 @@
+﻿using FlowerShopManagement.Application.MongoDB.Interfaces;
+using FlowerShopManagement.Core.Entities;
+
+namespace FlowerShopManagement.Application.Interfaces.MongoDB;
+
+public interface IImportRepository : IBaseRepository<GoodsReceivedNote>
+{
+}
---
+++ FlowerShopManagement.Infrustructure/MongoDB/Implements/ImportRepository.cs
@@ -0,0 +1,14 @@
+﻿using FlowerShopManagement.Application.Interfaces.MongoDB;
+using FlowerShopManagement.Core.Entities;
+using FlowerShopManagement.Infrustructure.MongoDB.Interfaces;
+
+namespace FlowerShopManagement.Infrustructure.MongoDB.Implements;
+
+public class ImportRepository : BaseRepository<GoodsReceivedNote>, IImportRepository
+{
+    public ImportRepository(IMongoDBContext mongoDbContext) : base(mongoDbContext) 
+    {
+        CreateUniqueIndex("_GRNNo");
+        CreateUniqueIndex("_PoNo");
+    }
+}
